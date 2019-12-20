1eb78f8b13213137f56e662177261afd34e2fafa
Janice Huang <janicehuang@gmail.com>
2019-12-20 13:57:41 -0800

    add note for future

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Feedback.css';
+// future improvement: use React Delay (already npm installed) to delay erasing (not posting) of these messages
 
 const Feedback = (props) => {
   const styleName = 'text_' + props.background + '_background'
